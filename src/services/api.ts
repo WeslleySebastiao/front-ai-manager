d6d988cae5957f2f3b5dc6ea110056ecee662468
Weslley Sebastião <weslleydacostasebastiao@gmail.com>
2025-11-12 20:24:41 -0300

    BUG: API backend nao funcionando

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://chatllm-api.vercel.app',
+  baseURL: 'chatllm-api.vercel.app',
   headers: {
     'Content-Type': 'application/json',
   },
